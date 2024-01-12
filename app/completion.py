259ace4a47ca68c041d15c30757b0ae1f6784c87
ddl-peter-lu <peter.lu@dominodatalab.com>
2024-01-12 11:11:51 -0800

    move def

--- app/completion.py
+++ app/completion.py
@@ -7,7 +7,7 @@ import openai
 import time
 from openai import OpenAI
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("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"))
 
 assistant = client.beta.assistants.retrieve("asst_0p9BODU7E2a1xd9rdqtwm7c4")
 
@@ -49,6 +49,15 @@ class OpenAIClient:
         if openai.api_type == "azure":
             self.openai_kwargs = {'engine': self.model}
 
+    def wait_on_run(run, thread):
+        while run.status == "queued" or run.status == "in_progress":
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread.id,
+                run_id=run.id,
+            )
+            time.sleep(0.5)
+        return run
+
     @backoff.on_exception(backoff.expo,
                           (openai.RateLimitError,
                            openai.APIConnectionError,
@@ -60,15 +69,6 @@ class OpenAIClient:
         else:
             return self.get_completion_text(prompt)
         
-
-    def wait_on_run(run, thread):
-        while run.status == "queued" or run.status == "in_progress":
-            run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id,
-            )
-            time.sleep(0.5)
-        return run
 
     def get_completion_chat(self, prompt) -> str:
         '''Invoke OpenAI API to get chat completion'''
